8ab439e08553018d08ea31c952f6aa2182839827
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2022-05-10 21:19:52 +0200

    Refined package structure

--- src/main/java/nathany3g/aidememoires/springboot/topics/openapi/TeaController.java
+++ src/main/java/nathany3g/aidememoires/springboot/topics/openapi/TeaController.java
@@ -1,4 +1,4 @@
-package nathany3g.aidememoires.springboot.openapi;
+package nathany3g.aidememoires.springboot.topics.openapi;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
